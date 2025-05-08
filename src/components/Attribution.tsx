bbac2152c1eeb620c2a99226713da687306c38aa
Ahtoh1337 <uvo1anton@gmail.com>
2025-05-08 15:03:06 +0300

    Add attribution styling

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -1,6 +1,6 @@
 export default function Attribution() {
-    return <footer>
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-            Coded by <a href="#">Your Name Here</a>.
+    return <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/Ahtoh1337">Ahtoh1337</a>.
     </footer>
 }
